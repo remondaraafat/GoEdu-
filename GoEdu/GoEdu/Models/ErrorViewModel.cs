887654a364ecf1754d2a0ccd991d3dc0d8f202a4
remondaraafat <remondarafat@gmail.com>
2025-03-31 21:49:32 +0200

    first commit

---
+++ GoEdu/GoEdu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoEdu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
