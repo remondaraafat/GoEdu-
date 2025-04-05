1865164d6d29561598cd893ac6bf16552b604f42
Christena1102002 <Krisayman13@gmail.com>
2025-04-05 20:57:29 +0200

    solve Merge Conflict

--- GoEdu/GoEdu/Interface/ICourseRepository.cs
+++ GoEdu/GoEdu/Interface/ICourseRepository.cs
@@ -7,15 +7,28 @@ namespace GoEdu.Repositories
     {
         public List<CourseViewModel> GetAllcourses();
         public List<Course> FilterCourses(string? instructorName, string? NameOfCourse);
-        //, string? specialization, string? sortBy
+        // , string? specialization, string? sortBy
 
         public List<Course> search(string searchQuery);
-<<<<<<< HEAD
         public CourseDetailsViewModel GetCourseWithLectures(int courseId);
-=======
 
         //david methods
         public List<Course> CoursesByInstructor(int instructorId);
->>>>>>> origin/main
     }
+
+    //        public List<CourseViewModel> GetAllcourses();
+
+    //        public List<Course> FilterCourses(string? instructorName, string? NameOfCourse);
+
+
+    //        public List<Course> search(string searchQuery);
+    //<<<<<<< HEAD
+    //        public CourseDetailsViewModel GetCourseWithLectures(int courseId);
+    //=======
+
+    //        //david methods
+    //        public List<Course> CoursesByInstructor(int instructorId);
+    //>>>>>>> origin/main
+    //    }
 }
+
--- GoEdu/GoEdu/obj/Debug/net8.0/GoEdu.AssemblyInfo.cs
+++ GoEdu/GoEdu/obj/Debug/net8.0/GoEdu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GoEdu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03757bf5f639f3a76e6880bee3227ae9c39a0165")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1d9b1a2a7820e0c5959b30bd6f630aaff7f7f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GoEdu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GoEdu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
